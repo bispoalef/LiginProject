a5b863c106dd298b10e6fc749360280f1fc2c324
bispoalef <alef.b.dos.santos.1993@gmail.com>
2022-09-09 22:16:33 -0300

    rotas nomeadas e as paginas iniciais

---
+++ login_project/lib/pages/splash.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue.shade700,
+          child: const Center(
+        child: CircularProgressIndicator(),
+      )),
+    );
+  }
+}
